b79216d0c328146242906cfa5e74c58e2a28f4b1
VShander <extrazone@yandex.ru>
2019-05-14 21:24:48 +0300

    ADD View from DB

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,12 +12,32 @@ class IndexController extends Controller {
 
     public function execute(Request $request) {
 
-        $pages = Page::all();
-        $portfolios = Portfolio::get(array('name', 'filter', 'images'));
-        $services = Service::where('id', '<', 20)->get();
-        $peoples = People::take(3)->get();
+        $pages = Page ::all();
+        $portfolios = Portfolio ::get(array('name', 'filter', 'images'));
+        $services = Service ::where('id', '<', 20) -> get();
+        $peoples = People ::take(3) -> get();
 
+        $menu = array();
+        foreach ($pages as $page) {
+            $item = ['title' => $page -> name, 'alias' => $page -> alias];
+            array_push($menu, $item);
+        }
 
-        return view('site.index');
+        $item = ['title' => 'Services', 'alias' => 'service'];
+        array_push($menu, $item);
+        $item = ['title' => 'Portfolio', 'alias' => 'Portfolio'];
+        array_push($menu, $item);
+        $item = ['title' => 'Team', 'alias' => 'team'];
+        array_push($menu, $item);
+        $item = ['title' => 'Contact', 'alias' => 'contact'];
+        array_push($menu, $item);
+
+        return view('site.index', [
+            'menu' => $menu,
+            'pages' => $pages,
+            'services' => $services,
+            'portfolios' => $portfolios,
+            'peoples' => $peoples
+        ]);
     }
 }
